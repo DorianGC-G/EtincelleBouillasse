675ba989fb4aee3652f29ed6101433149eb8c085
DorianGC-G <dorian.guitel@gmail.com>
2022-05-26 14:36:50 +0200

    carousel auto changes images

---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "img1", "img2", "img3", "img4", "img5" ]
+
+  connect() {
+    let imgNum = 0
+    const imgTargets = [
+      this.img1Target, 
+      this.img2Target, 
+      this.img3Target, 
+      this.img4Target, 
+      this.img5Target
+    ]
+    setInterval(() => {
+      imgTargets[imgNum].checked = true
+      console.log(imgTargets.length, imgNum)
+      imgNum < (imgTargets.length - 1) ? imgNum += 1 : imgNum = 0
+    }, 5000)
+  }
+
+}
