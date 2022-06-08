9d8e8cc746115149ea469d63f04b208ed4f1a8d3
DorianGC-G <dorian.guitel@gmail.com>
2022-06-08 12:07:35 +0200

    changed carousel images display

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -3,19 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "img1", "img2", "img3", "img4", "img5" ]
 
-  connect() {
-    let imgNum = 0
-    const imgTargets = [
-      this.img1Target, 
-      this.img2Target, 
-      this.img3Target, 
-      this.img4Target, 
-      this.img5Target
-    ]
-    setInterval(() => {
-      imgTargets[imgNum].checked = true
-      imgNum < (imgTargets.length - 1) ? imgNum += 1 : imgNum = 0
-    }, 5000)
-  }
+  // connect() {
+  //   let imgNum = 0
+  //   const imgTargets = [
+  //     this.img1Target, 
+  //     this.img2Target, 
+  //     this.img3Target, 
+  //     this.img4Target, 
+  //     this.img5Target
+  //   ]
+  //   setInterval(() => {
+  //     imgTargets[imgNum].checked = true
+  //     imgNum < (imgTargets.length - 1) ? imgNum += 1 : imgNum = 0
+  //   }, 5000)
+  // }
 
 }
