51750e1855d01abb9af72d941fdbc762c82dca33
DorianGC-G <dorian.guitel@gmail.com>
2022-06-09 11:03:14 +0200

    added image preview in biographie / spectacle new and edit

--- app/javascript/controllers/forms_controller.js
+++ app/javascript/controllers/forms_controller.js
@@ -1,20 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "btn", "atHomeRadio", "outsideRadio", "otherLocationInput" ]
+  static targets = [ "btn", "atHomeRadio", "outsideRadio", "otherLocationInput", "gallery", "preview" ]
   
   initialize() {
     this.imgNum = 1
   }
 
+  addPreviewImage() {
+    let src = this.galleryTarget.getElementsByTagName('input')
+    console.log(src)
+    this.previewTarget.insertAdjacentHTML('beforeend', `<img src="${src[this.imgNum - 2].value}">`)
+  }
+
   addBiographyImageInput() {
     this.imgNum += 1
     this.btnTarget.insertAdjacentHTML('beforebegin', `<label for="biographie_Image ${this.imgNum}:">Image ${this.imgNum}:</label><br><input type="text" name="biographie[gallery][image_${this.imgNum}]" id="biographie_gallery_image_${this.imgNum}"><br><br>`)
+    this.addPreviewImage()
   }
 
   addSpectacleImageInput() {
     this.imgNum += 1
     this.btnTarget.insertAdjacentHTML('beforebegin', `<label for="spectacle_Image ${this.imgNum}:">Image ${this.imgNum}:</label><br><input type="text" name="spectacle[gallery][image_${this.imgNum}]" id="spectacle_gallery_image_${this.imgNum}"><br><br>`)
+    this.addPreviewImage()
   }
 
   displayLocationInput() {
